f293aff596c1417f36600190181d6cc56fd926f7
gamblerLt <rimas.spakauskas@outlook.com>
2022-11-30 19:51:42 +0200

    elseSwitchHome

---
+++ src/task/SecondTask.java
@@ -0,0 +1,31 @@
+package task;
+
+public class SecondTask {
+    public static void main(String[] args) {
+        int s = 8;
+
+        if (s <= 0) {
+            System.out.println("Netinkamas skaicius");
+        } else if (s >= 13) {
+            System.out.println("Netinkamas skaicius");
+        } else if (s % 2 != 1) {
+            System.out.println("Lyginis");
+
+        } else {
+            System.out.println("Nelyginis");
+        }
+        switch (s) {
+            case 1 -> System.out.println("Sausis");
+            case 2 -> System.out.println("Vasaris");
+            case 3 -> System.out.println("Kovas");
+            case 4 -> System.out.println("Geguze");
+            case 6 -> System.out.println("Birzelis");
+            case 7 -> System.out.println("Liepa");
+            case 8 -> System.out.println("Rugpjutis");
+            case 9 -> System.out.println("Rugsejis");
+            case 10 -> System.out.println("Spalis");
+            case 11 -> System.out.println("Lapkritis");
+            case 12 -> System.out.println("Gruodis");
+        }
+    }
+}
