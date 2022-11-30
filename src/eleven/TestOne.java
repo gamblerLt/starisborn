066e50340e291b7f8d40f76528174c4b31260cfb
gamblerLt <rimas.spakauskas@outlook.com>
2022-11-30 16:33:27 +0200

    Pirmas kelimas

---
+++ src/eleven/TestOne.java
@@ -0,0 +1,11 @@
+package eleven;
+
+public class TestOne {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world! Keliu is ausros!");
+        System.out.println("2 versija!");
+    }
+}
+
