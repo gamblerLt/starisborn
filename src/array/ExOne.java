2858ecc1e3be629044074aad63546093448d45ca
gamblerLt <rimas.spakauskas@outlook.com>
2022-12-01 21:17:15 +0200

    Update ExOne.java

--- src/array/ExOne.java
+++ src/array/ExOne.java
@@ -1,4 +1,11 @@
 package array;
 
 public class ExOne {
+    public static void main(String[] args) {
+        for (int j = 22; j <= 30; j++) {
+            System.out.println("jj" + j);
+        }
+
+    }
 }
+
