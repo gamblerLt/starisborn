0128d422755cf353a78b77974239d369f0ac1b8e
gamblerLt <rimas.spakauskas@outlook.com>
2022-12-01 21:52:46 +0200

    Update ExOne.java

--- src/array/ExOne.java
+++ src/array/ExOne.java
@@ -1,11 +1,34 @@
 package array;
 
-public class ExOne {
-    public static void main(String[] args) {
-        for (int j = 22; j <= 30; j++) {
-            System.out.println("jj" + j);
-        }
+import java.util.Scanner;
 
+public class ExOne {
+//    public static void main(String[] args) {
+//        for (int j = 1; j <= 5; j++) {
+//            System.out.println("jj" + j);
+//        }
+//
+//    }
+    //
+    public static void main(String[] args){
+        System.out.println("iveskite vardus");
+        var names = getNames();
+
+        for (var j = 0; j < names.length; j++){
+            var currentName = names[j];
+            var modifiedName = currentName.toUpperCase() + "-" + currentName.length() + "-" + j;
+            System.out.println(modifiedName);
+        }
+    }
+    static String[] getNames(){
+        var scanner = new Scanner(System.in);
+        var names = new String[10];
+
+        for (var i = 0; i <= 9; i++){
+          var name = scanner.nextLine();
+            names [i] = name;
+        }
+        return names;
     }
 }
 
