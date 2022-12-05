b2219c623e81d4197f8e96fcf06d9e397eaf8912
gamblerLt <rimas.spakauskas@outlook.com>
2022-12-05 18:48:44 +0200

    13 paskaita

--- src/array/MasExamples.java
+++ src/array/MasExamples.java
@@ -6,16 +6,16 @@ public class MasExamples {
 
         // masyvus galima sukurti 3 budais
 
-        int[] mas = new int[10];
-        int another [] new int [10]; //sito nenaudoti
-        int[] thirdMass = {0, 1, 2, 3}; // sitas naudojamas kai jau tikrai zinai reiksmes ir nelabai naudojamas
+      //  int[] mas = new int[10];
+       // int another [] new int [10]; //sito nenaudoti
+        int[] thirdMass = {0, 1, 2, 3, 4, 5}; // sitas naudojamas kai jau tikrai zinai reiksmes ir nelabai naudojamas
 
         System.out.println(thirdMass[2]); // atspausdins trecia reiksme
         System.out.println(thirdMass[thirdMass.length - 1]); // parodys paskutine reiksme
 
         //masyvui priskirsime reiksme:
 
-        mas[2] = 0;
+        //mas[2] = 0;
 
     }
 }
--- src/task/ForExample.java
+++ src/task/ForExample.java
@@ -1,7 +1,18 @@
 package task;
 
 public class ForExample {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        // i for sintakse ieina. turi buti zingsniai pradios ir pabaigos alygos
+        for (int i = 0; i < 10; i++) {
+            System.out.println("sss");
+        }
+        int[] mas = {5, 4, 8, 9, 6, 7,};
+        for (int i = 0; i < mas.length; i++) {
+            //spausdina visa masyva
+            System.out.println(mas[i]);
+
+
+        }
     }
 }
