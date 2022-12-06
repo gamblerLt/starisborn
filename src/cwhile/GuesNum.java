bc7b8e34c47371e9e58015a7a097ef8645a2826b
gamblerLt <rimas.spakauskas@outlook.com>
2022-12-06 16:25:52 +0200

    Update GuesNum.java

--- src/cwhile/GuesNum.java
+++ src/cwhile/GuesNum.java
@@ -1,6 +1,7 @@
 package cwhile;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Random;
 import java.util.InputMismatchException;
 public class GuesNum {
     public static void main(String[] args) {
@@ -11,33 +12,28 @@ public class GuesNum {
         Arrays.setAll(mas, i -> i + 1);
         System.out.println("Masyvo skaiciai yra sie: " + Arrays.toString(mas));
 
+        //generuoti random
+        System.out.println("Atsitiktinis masyvo skaicius yra: " + mas[new Random().nextInt(mas.length)]);
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Iveskite sk: ");
-       /* try {
-            System.out.println("Iveskite skaiciu nuo 1 iki 100:");
-            int i = scanner.nextInt();
+        System.out.println("Atspekite atsitiktini skaiciu: ");
 
-         //   System.out.println("Jusu ivestas skaicius yra " + i);
-        } catch (InputMismatchException ex) {
-
-            scanner.nextLine();
-
-            //System.out.println("Jusu ivesto skaiciaus masyve nera. Iveskite kita skaiciu" + mas.length);
-        } while (n <= 100);
-        System.out.println("Iveskite  100 ar maziau");*/
         int i = scanner.nextInt();
 
-
-        while(i < 10){
-            System.out.println("Iveskite sk");
-            i++;
+        int num = 0;
+        for(int j = 0; j < mas.length; j++){
+            if(mas[j] == i){
+                num = mas[i];
+                break;
+            }
+       // if (i == mas[new  Random().nextInt(mas.length)]) {
+            System.out.println("Atspejote");
+            //i++;
         }
-        do
-            System.out.println(i);
+        if(num > 0) {
+            System.out.println(num);
+        }
+          //  while (i== mas[new Random().nextInt(mas.length)]);
 
-        while (i < 10);
-
-
-
+        }
     }
-}
