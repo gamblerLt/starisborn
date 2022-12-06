cfb0269bff7554383b6bf42142f5fc82428d314d
gamblerLt <rimas.spakauskas@outlook.com>
2022-12-06 21:57:05 +0200

    update

--- src/task/Lottery.java
+++ src/task/Lottery.java
@@ -1,4 +1,27 @@
 package task;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+/*
+/Para6ykite program1, kuri nustato ar loterijosbilietas yralaimingas.
+/Loterijos bilieto 6 zenklis numeris laikomas laimingu, jeigu jo pirmuju triju skiciu suma
+/lygi paskutiniu triju skaiciu sumai.
+/Bilieto numeri perduokite komandineje eiluteje. pvz 968745
+ */
 public class Lottery {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Iveskite sesiazenkli skaiciu:");
+        int ticket = scanner.nextInt();
+
+
+     //   private boolean isSumEqual ( int sum){
+
+       // }
+    }
 }
+//String[] mass = "1234566".split("");
