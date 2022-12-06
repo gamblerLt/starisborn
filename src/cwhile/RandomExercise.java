3ebb53d2c21a228d43c4fda5d6e5ba0ce4d933be
gamblerLt <rimas.spakauskas@outlook.com>
2022-12-06 15:22:23 +0200

    work

--- src/cwhile/RandomExercise.java
+++ src/cwhile/RandomExercise.java
@@ -3,7 +3,10 @@ import java.util.Random;
 public class RandomExercise {
     public static void main(String[] args) {
 
+        RandomExercise exercice = new RandomExercise();
         int[] mas = {5, 10, 15};
+        int max = exercice.getMax(mas);
+        //System.out.println("Maksimalus masyvo sk. yra: " + mas);
 
         Random random = new Random();
         int a = random.nextInt(100) + 1;
@@ -11,13 +14,23 @@ public class RandomExercise {
         int c = random.nextInt(100) + 1;
 
 
-
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
 
-        System.out.println("Maksimalusskaicius: " + Math.max(a, c));
+        System.out.println("Maksimalus masyvo sk. yra: " + mas);
 
+      System.out.println("Maksimalusskaicius: " + Math.max(a, c));
+
+    }private int getMax(int[] mas){
+        int max = mas[0];
+        for(int i = 1; i > mas.length; i++){
+            int value = mas[i];
+            if(value > max){
+                max = value;
+            }
+        }
+        return  max;
     }
 
     }
