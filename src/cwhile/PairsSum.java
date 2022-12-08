9104efdb8cb16b05fa87906f543fd1cd3724585b
gamblerLt <rimas.spakauskas@outlook.com>
2022-12-08 16:31:27 +0200

    wurk7d

---
+++ src/cwhile/PairsSum.java
@@ -0,0 +1,22 @@
+/*
+package cwhile;
+
+public class PairsSum {
+    public static void main(String[] args) {
+
+
+    pairValue(new int[] {2, 7, 4, -5, 11, 5, 20}, 15);
+    pairValue(new int[]{14, -15, 9, 16, 25, 45, 12, 8}, 30);
+}
+static void pairValue(int array, int number){
+    System.out.println("Suma: ");
+        for(int i = 0; i < array.length; i++){
+                for(int j = i + 1; j < array.length; j++){
+                    if(array[i] + array[j] == number){
+                        System.out.println(array[i] + "+" + array[j] + "=" + number);
+                    }
+                }
+        }
+}
+}
+*/
---
+++ src/kidding/Meths.java
@@ -0,0 +1,17 @@
+package kidding;
+
+public class Meths {
+    public static void main(String[] args) {
+
+        kintamieji(500, 9500, 851);
+
+            }
+    public static  void kintamieji(int ilgis, int plotis, int aukstis){
+
+        var sum = ilgis + plotis;
+        System.out.println(sum);
+    }
+
+
+
+}
