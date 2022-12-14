9e312b54f3a95bb231c7cbea3a57595033301060
gamblerLt <rimas.spakauskas@outlook.com>
2022-12-14 22:04:49 +0200

    kolekcijosStr

---
+++ src/collection/ColStr.java
@@ -0,0 +1,18 @@
+package collection;
+
+public class ColStr {
+    public static void main(String[] args) {
+
+    }
+}
+/*
+* Sukurti String tipo elementus galintį saugoti sąrašą
+Vartotojas įvedinėja žodžius, kuriuos programa deda į
+sąrašą
+Po įvedimo atspausdinamas visas sąrašas
+Jei sąrašo dydis tampa 5, tada sąrašas išvalomas ir toliau
+dedama į tuščią sąrašą
+Jei dedant į sąrašą paaiškėja, kad toks elementas jau yra,
+tada spausdinamas klaidos pranešimas ir toliau laukiama
+kitų žodžių
+* */
