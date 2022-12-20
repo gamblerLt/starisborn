45cd7b87d00724590a02e0e8955dffdf55c703a2
gamblerLt <rimas.spakauskas@outlook.com>
2022-12-20 16:43:10 +0200

    20dwurk

--- src/collection/list/set/Cat.java
+++ src/collection/list/set/Cat.java
@@ -9,11 +9,10 @@ public class Cat implements  Comparable<Cat>{
         this.name = name;
         this.age = age;
 
-
     }
-
-
 //desinis klavisas, generate, tostring
+
+
     @Override
     public String toString() {
         return "Cat{" +
@@ -29,3 +28,40 @@ public class Cat implements  Comparable<Cat>{
     }
 }
 //2 val 12 min kazka dar sukure
+/*
+* @Override
+	public boolean equals(Object o) {
+		if(this == o)
+		{
+			return true;
+		}
+		if(o == null || getClass() != o.getClass())
+		{
+			return false;
+		}
+		Cat cat = (Cat) o;
+		return age == cat.age && name.equals(cat.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, age);
+	}
+
+	@Override
+	public String toString() {
+		return "Cat{" +
+			   "name='" + name + '\'' +
+			   ", age=" + age +
+			   '}';
+	}
+
+	@Override
+	public int compareTo(Cat o) {
+		if(name.equals(o.name)) {
+			if(age == o.age) {
+				return 0;
+			}
+			return age > o.age ? 1 : -1;
+		}
+		return name.compareTo(o.name);*/
