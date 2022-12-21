b7c9b70898f624bb0126116b229fb326b0deecd6
gamblerLt <rimas.spakauskas@outlook.com>
2022-12-21 22:14:44 +0200

    Home21dnightmare

--- src/collection/EncodeClass.java
+++ src/collection/EncodeClass.java
@@ -1,5 +1,9 @@
 package collection;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class EncodeClass {
     //reikes meniu, uzkoduoti, atkoduoti
 
@@ -9,15 +13,121 @@ public class EncodeClass {
             "99 c;116 t;56 8;77 M;100 d;117 u;57 9;78 N;101 e;118 v;63 ?;79 O;102 f;119 w;80 P;103 g;120 x;" +
             "81 Q;104 h;121 y;122 z";
 
+ //4
+ //puriame konstanta
+    private static  final String SPACE = " ";
+
+
+    //2
+    //kuriame 2 klases kintamuosius:
+    //1kintamasis tipo tokiam 12  a, ty Key yra raide value yra koduote
+    private final Map<String, String> encodes = new HashMap<>();
+
+    //1kintamasis tipo tokiam a   12, ty key yra koduote, o value raide
+    private final Map<String, String> decodes = new HashMap<>();
+
+
     public static void main(String[] args) {
+//5
+        EncodeClass encodedecode = new EncodeClass();
+        encodedecode.fillUpMaps();
+
+        Scanner sc = new Scanner(System.in);
+
+        String action;
+
+        do {
+            encodedecode.menu();
+            action = sc.nextLine();
+            encodedecode.selectAction(sc, action); // sitas eina case 3 //7
+        }while(!action.equals("3"));
+
+    }
+    //6
+        private void selectAction(Scanner sc, String action){
+        switch (action){
+            case "1" -> encode(sc);
+            case "2" -> decode(sc);
+            case "3" -> System.out.println("EXIT");
+            default -> System.out.println("nera tokio");
+        }
+    }
+    //8
+    //8.2.
+    private void encode(Scanner sc){
+        //Labas sita koduosiu teksta
+        System.out.println("Iveskite uzkoduota teksta");
+        String text = sc.nextLine();
+
+        //9>>
+        StringBuilder sb = new StringBuilder();
+
+        /*for(int i = 0; i > text.length(); i++){
+            String letter = String.valueOf(text.charAt(i));*/
+        for(char c: text.toCharArray()){
+            String letter =  String.valueOf(c);
+            //yra kitas variantas pas andriu
+
+            String code = encodes.get(letter);
+            if (code == null){
+                System.out.println("Tokios raides koduotes nera" + letter);
+                continue;
+            }
+            //9
+            sb.append(code).append(SPACE);
+
+        }
+        System.out.println("uzkoduotas tekstas yra: "+ sb.toString().trim());
+
 
     }
 
+    //8.1.
+    private void decode(Scanner sc){
+        System.out.println("Iveskite norima dekoduoti koduote");
+        String line = sc.nextLine();
+        String[] splits = line.split(SPACE);
+
+        StringBuilder sb = new StringBuilder();
+
+        for(String s: splits){
+            String letter = decodes.get(s);
+            if(letter == null){
+                System.out.println("Neegzistuoja koduote" + s);
+                continue;
+            }
+            sb.append(letter);
+        }
+        System.out.println("Vertimas: " + sb);
+
+    }
+
+
+//1
     private void menu(){
         System.out.println("""
+                <<<Meniu>>>
                 1. Encode
                 2. Decode
+                3. Exit
                 """);
     }
+  //3
+  //reikiametodo:
+  private void fillUpMaps(){
+        String[] pairs = CODES.split(";");
+        for(String pair: pairs){
+            String[] splitPairs = pair.split(SPACE);
+
+            if(splitPairs.length != 2){
+                continue;
+            }
+            String code = splitPairs[0];
+            String letter =splitPairs[1].equals("tarpas") ? SPACE : splitPairs[1];
+
+            encodes.put(letter, code);
+            decodes.put(code, letter);
+        }
+  }
 
 }
---
+++ src/collection/StudentTest.java
@@ -0,0 +1,91 @@
+package collection;
+
+import java.util.*;
+
+public class StudentTest {
+
+    private static final String QUESTIONS = "Kas yra obuolys?,Gyvunas,Pastatas,Vaisius,3;Ka geria karve?,Piena,Alu,Vandeni,3;Is ko gaminamos pomidoru sultys?,Is morku,Is pomidoru,Is vistienos,2;" +
+            "Kas yra vista?,Gyvunas,Pastatas,Vaisius,1;Ka eda jautis?,Siena,Kepsnius,Cipsus,1;Is ko gaminami kiausiniai?,Is vistu,Is pomidoru,Is bulviu,1;" +
+            "Kas yra namas?,Gyvunas,Pastatas,Vaisius,2;Ka eda katinas?,Siena,Kepsnius,Cipsus,2;Is ko gaminami megztiniai?,Is vilnos,Is pomidoru,Is bulviu,1;" +
+            "Kas yra fosa?,Gyvunas,Pastatas,Vaisius,1;Ka duoda karve?,Piena,Alu,Vandeni,1;Is ko gaminamos morku sultys?,Is morku,Is pomidoru,Is vistienos,1;" +
+            "Kas yra bunkeris?,Gyvunas,Pastatas,Vaisius,2;Ka geria antis?,Piena,Alu,Vandeni,3;Is ko gaminami cepelinai?,Is morku,Is pomidoru,Is bulviu,3";
+
+    private final Map<Integer, Question> questions = new HashMap<>();
+    int count = 1;
+    public static void main(String[] args) {
+        StudentTest task = new StudentTest();
+        task.fillUpQuestionsMap();
+        Set<Question> questionsForUser = task.getQuestionsForUser();
+
+       int mark = task.getStudentMark(questionsForUser);
+       //todo gera > 8 < 5 vidutiniskai 5 ir tt
+        System.out.println("Jusu pazymys: " +mark);
+        if(mark <= 3){
+            System.out.println("Jusu pazymys neigiamas (>=3)");
+        }else if(mark < 7) {
+            System.out.println("Neblogas pasirodymas");
+        } else if (mark <=10) {
+            System.out.println("Puiku");
+                    }else{
+
+        System.out.println("Kazkas ne taip ...");
+        }
+
+
+    }
+    private int getStudentMark(Set<Question>questionsForUser){
+        Scanner sc = new Scanner(System.in);
+        int questionCount = 1;
+        int mark = 0;
+        for(Question q: questionsForUser){
+            System.out.println(questionCount++ + q.question());
+            int aCount = 1;
+            for(String a: q.answers()){
+                System.out.println(aCount++ + a);
+            }
+            int userAnswer = sc.nextInt(); // todo kad neivestu stringo arba didesnio skaiciaus nei yra atsakymu
+            if(userAnswer == q.correctAnswer()){
+                mark++;
+            }
+
+        }
+        return mark;
+    }
+
+    private Set<Question>getQuestionsForUser(){
+        Set<Question> uniqueQuestions = new LinkedHashSet<>();
+
+        Random random = new Random();
+
+        while(uniqueQuestions.size() < 10) {
+
+            int numb = random.nextInt(15) + 1;
+            Question question = questions.get(numb);
+            if (question != null) {
+                uniqueQuestions.add(question);
+            }
+        }
+
+        return uniqueQuestions;
+    }
+
+    //reikia metodo uzpildyti mapa
+    private void fillUpQuestionsMap(){
+        String[] questionsSplit = QUESTIONS.split(";");
+        for (String q: questionsSplit){
+            //q -.. Kas yra obuolys? ...Vaisius, 3
+            System.out.println(q);
+            String[] qSplit = q.split(",");
+            //paaiskinimai pas andriu faile
+            List<String> answers = Arrays.stream(qSplit).toList().subList(1, qSplit.length -1);
+
+
+
+            Question question = new Question(qSplit[0], answers, Integer.parseInt(qSplit[qSplit.length - 1]));
+
+            questions.put(count++, question);
+        }
+
+
+    }
+}
