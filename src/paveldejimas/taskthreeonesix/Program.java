fab8dfd6ec0810039c1305781785779eb3eacbc5
gamblerLt <rimas.spakauskas@outlook.com>
2023-01-02 16:08:17 +0200

    230102

---
+++ src/paveldejimas/taskthreeonesix/Program.java
@@ -0,0 +1,44 @@
+package paveldejimas.taskthreeonesix;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Storage storage = new Storage() {
+            @Override
+            public void saveInfo() {
+
+            }
+
+            @Override
+            public int findInfo(int Id) {
+                return 0;
+            }
+
+            @Override
+            public String findStringInfo(String textInfo) {
+                return null;
+            }
+        };
+
+    }
+}
+/*Klasėje taip pat turi būti dar trys statiniai metodai
+• Metodas saugoti • Nieko negrąžina, bet priima Saugykla ir Info parametrus
+• Metodas iškviečia Saugyklos metodą saugotiInfo
+• Metodas rastiPagalId
+• Nieko negrąžina, bet priima Saugykla ir id parametrus
+• Metodas iškviečia Saugyklos metodą rastiInfo
+• Atspausdina rastą Info objektą
+• Metodas rastiPagalZodi
+• Nieko negrąžina, bet priima Saugykla ir zodis parametrus
+• Metodas iškviečia Saugyklos metodą rastiInfo
+• Atspausdina rastą Info objektą
+
+
+
+Main metode sukurti dviejų tipų saugyklas – nutolusį diską ir
+duomenų bazę
+• Sukurti keletą Info tipo objektų
+• Iškviesti saugoti, rastiPagalid ir rastiPagalZodi metodus
+
+*/
--- src/paveldejimas/taskthreeonesix/RemoteDisk.java
+++ src/paveldejimas/taskthreeonesix/RemoteDisk.java
@@ -17,3 +17,16 @@ public class RemoteDisk implements Storage{
         return null;
     }
 }
+
+/*
+* Sukurti klasę NutolesDiskas, kuri įgyvendina Saugykla interfeisą
+• Klasė turi turėti ArrayList, skirtą Info objektų saugojimui
+• saugotiInfo metodas išsaugo gautą Info objektą į ArrayList
+• Pirmasis rastiInfo metodas gauna Info iš sąrašo iteruodamas kiekvieną
+elementą ir tikrindamas ar per parametrus gautas id yra lygus Info objekto id.
+Grąžina pirmą rastą Info objektą
+• Antrasis rastiInfo metodas gauna Info iš sąrašo iteruodamas kiekvieną
+elementą ir tikrindamas ar per parametrus gautas žodis nėra Info objekto
+tekste. Grąžina pirmą rastą Info objektą
+• Visi metodai atspausdina kas buvo padaryta: „Issaugota i nutolusi diska“, „Rasta
+nutolusiame diske pagal id“, „Rasta nutolusiame diske pagal zodi*/
