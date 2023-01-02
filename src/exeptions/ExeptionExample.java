61cb50d279fc9fd68addaa0b1cd3676925dd3be3
gamblerLt <rimas.spakauskas@outlook.com>
2023-01-02 20:52:47 +0200

    tryCatchExeption

--- src/exeptions/ExeptionExample.java
+++ src/exeptions/ExeptionExample.java
@@ -1,5 +1,6 @@
 package exeptions;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ExeptionExample {
@@ -9,6 +10,28 @@ public class ExeptionExample {
         try {
             int value = scanner.nextInt();
             System.out.println(value);
-        } catch ()
+        } catch (InputMismatchException e) {
+            System.out.println("Nezinoma klaida");
+        }
+        ExeptionExample example = new ExeptionExample();
+        try {
+            example.testExeption();
+        } catch (Exception e) {
+            System.out.println("jsdbvka");
+        }finally {
+            System.out.println("finally kviecamas visada, nes kai programa pastringa rasom pvz svaner close");
+            scanner.close();
+        }
+    }
+    private  void testExeption() throws Exception {
+        //meata klaida su throw zodeliu. turime ideti i try catck bloka arba mesti toliau,
+        // todel prisideda zodelis throws - checked exeptiom arba compile exeption
+        throw  new Exception("this is exeption");
+    }
+
+    //unchecked exeption arba runtime exeption - tai naudojama, kai nera kritine klaida, pvz  kai vartotojas praleidzia ka
+    //nors ivesti ir programa del to neturetu sustoti
+    private  void testRuntimeExeption(){
+        throw new RuntimeException("This is runtimeexeption");
     }
 }
