ecbb9d8324d7d3284bfd03dc3f01e57a69391907
gamblerLt <rimas.spakauskas@outlook.com>
2023-01-02 21:59:14 +0200

    230102exeption

---
+++ src/exeptions/CustomExeption.java
@@ -0,0 +1,9 @@
+package exeptions;
+/*Savo apsibrėžta išimties klasė
+• Reikalingas, kai norime specializuoti išimtį, kad klientinė dalis
+galetų išskirti ją iš kitų situacijų ir atitinkamai reaguotų
+• Kai norime papildyti Exception klasę specifine informacija
+*/
+public class CustomExeption extends Exception{
+
+}
--- src/exeptions/ExeptionExample.java
+++ src/exeptions/ExeptionExample.java
@@ -1,6 +1,7 @@
 package exeptions;
 
 import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class ExeptionExample {
@@ -18,20 +19,45 @@ public class ExeptionExample {
             example.testExeption();
         } catch (Exception e) {
             System.out.println("jsdbvka");
-        }finally {
+        } finally {
             System.out.println("finally kviecamas visada, nes kai programa pastringa rasom pvz svaner close");
             scanner.close();
         }
+
+        try {
+            System.out.println(scanner.nextInt());
+        } catch (NullPointerException e) {
+
+        } catch (NoSuchElementException e) {
+
+        } catch (RuntimeException e) {
+
+        } catch (Exception e) {
+        }
+        //kostimizuota klaida
+        try {
+            example.testCustomExeption();
+        } catch (CustomExeption e) {
+            System.out.println("Custom exeption");
+        }
+
+
+
     }
-    private  void testExeption() throws Exception {
-        //meata klaida su throw zodeliu. turime ideti i try catck bloka arba mesti toliau,
-        // todel prisideda zodelis throws - checked exeptiom arba compile exeption
-        throw  new Exception("this is exeption");
+        private void testExeption () throws Exception {
+            //meata klaida su throw zodeliu. turime ideti i try catck bloka arba mesti toliau,
+            // todel prisideda zodelis throws - checked exeptiom arba compile exeption
+            throw new Exception("this is exeption");
+        }
+
+        //unchecked exeption arba runtime exeption - tai naudojama, kai nera kritine klaida, pvz  kai vartotojas praleidzia ka
+        //nors ivesti ir programa del to neturetu sustoti
+        private void testRuntimeExeption () {
+            throw new RuntimeException("This is runtimeexeption");
+
+        }
+        private  void testCustomExeption() throws CustomExeption {
+        throw new CustomExeption();
+        }
     }
 
-    //unchecked exeption arba runtime exeption - tai naudojama, kai nera kritine klaida, pvz  kai vartotojas praleidzia ka
-    //nors ivesti ir programa del to neturetu sustoti
-    private  void testRuntimeExeption(){
-        throw new RuntimeException("This is runtimeexeption");
-    }
-}
