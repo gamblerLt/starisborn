7448042aae381efc517fc188b1a0b675756b6ce2
gamblerLt <rimas.spakauskas@outlook.com>
2023-01-02 11:41:59 +0200

    2Diena2

--- src/paveldejimas/taskthreeonesix/DataBase.java
+++ src/paveldejimas/taskthreeonesix/DataBase.java
@@ -3,6 +3,7 @@ package paveldejimas.taskthreeonesix;
 import java.security.Key;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class DataBase implements Storage{
 
@@ -12,23 +13,37 @@ public class DataBase implements Storage{
 
     @Override
     public void saveInfo() {
+
         HashMap<Integer, String> information = new HashMap<>();
-        information.put(1,"Vienas");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Įveskite ID");
+        int id = Integer.parseInt(sc.nextLine());
+        System.out.println("Įveskite tekstą");
+        String text = sc.nextLine();
+
+
+        /*information.put(1,"Vienas");
         information.put(2, "Du");
         information.put(3, "Trys");
         information.put(4, "Keturi");
-        information.put(5, "Penki");
+        information.put(5, "Penki");*/
     }
 
     @Override
     public int findInfo(int Id) {
-     for (Map.Entry<Integer, String> entry : information.entryset() ){
-         System.out.println((entry.getKey() + " = " + entry.getValue()));
-     }
 
-        /*for( Map.Entry<Integer, String> entry : vehicles.entrySet() ){
-    System.out.println( entry.getKey() + " = " + entry.getValue() );
-}*/
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Iveskite Key reikšmę patikrinti ar tokia egzistuoja:");
+        int key = scanner.nextInt();
+        if (information.containsKey(key)){
+            System.out.println("Toks raktas yra!");
+        }else{
+
+        }
+        return 0;
+
+
     }
 
     @Override
