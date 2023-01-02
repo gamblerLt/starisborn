faaa4238d7525e56e5db0056863e64be7aee3d9b
gamblerLt <rimas.spakauskas@outlook.com>
2023-01-02 16:25:47 +0200

    23.01.02.16.25

--- src/paveldejimas/taskthreeonesix/DataBase.java
+++ src/paveldejimas/taskthreeonesix/DataBase.java
@@ -5,84 +5,81 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class DataBase implements Storage{
-
-    HashMap<Integer, String> information = new HashMap<Integer, String>();
-    boolean loopAgain = true;
-    Scanner sc = new Scanner(System.in);
-
-    do{
-
-        System.out.print("Įveskite ID");
-        Integer id = Integer.parseInt(sc.nextLine());
-
-        System.out.println("Įveskite tekstą");
-        String text = sc.nextLine();
-
-        String userInput = information.put(id, text);
-        if (userInput != null) {
-            System.out.println("Id =" + id + userInput + "Perrašyta į " + text);
-        }
-        System.out.println("Ar dar įvesite daugiau duomenų? T / N ");
-        String answer = sc.nextLine();
-
-        if (answer.equals("y") || answer.equals("Y")) {
-            continue;
-
-        }else {
-
-            break;
-        }
-        while (loopAgain) ;
-        sc.close();
-
-        System.out.println("Duomenu bazeje yra sie duomenys: " + information.get(id) + " " + information.get(text));
-
-
-    }
-
-
-
+public class DataBase implements Storage {
 
     @Override
     public void saveInfo() {
+
+        HashMap<Integer, String> information = new HashMap<Integer, String>();
+        boolean loopAgain = true;
+        Scanner sc = new Scanner(System.in);
+
+        do {
+
+            System.out.print("Įveskite ID");
+            Integer id = Integer.parseInt(sc.nextLine());
+
+            System.out.println("Įveskite tekstą");
+            String text = sc.nextLine();
+
+            String userInput = information.put(id, text);
+            if (userInput != null) {
+                System.out.println("Id =" + id + userInput + "Perrašyta į " + text);
+            }
+            System.out.println("Ar dar įvesite daugiau duomenų? T / N ");
+            String answer = sc.nextLine();
+
+            if (answer.equals("y") || answer.equals("Y")) {
+                continue;
+
+            } else {
+
+                break;
+            }
+            while (loopAgain) ;
+            sc.close();
+
+            System.out.println("Duomenu bazeje yra sie duomenys: " + information.get(id) + " " + information.get(text));
+        }
+
 
         /*information.put(1,"Vienas");
         information.put(2, "Du");
         information.put(3, "Trys");
         information.put(4, "Keturi");
         information.put(5, "Penki");*/
-    }
-
-    @Override
-    public int findInfo(int Id) {
-
-        //Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Iveskite Key reikšmę patikrinti ar tokia egzistuoja:");
-        int key = sc.nextInt();
-        if (information.containsKey(key)){
-            System.out.println("Toks raktas yra!");
-        }else{
-            System.out.println("Tokio rakto nėra, įveskite kitą raktą:");
-        }
-        return sc.nextInt();
 
 
-    }
+        @Override
+        public int findInfo ( int Id){
 
-    @Override
-    public String findStringInfo(String textInfo) {
-        // Scanner scanner = new Scanner(System.in);
-        System.out.println("Kokio žodžio ieškote?");
-        String value = sc.nextLine();
-        if (information.containsValue(value)) {
-            System.out.println("Toks žodis yra");
-        } else {
-            System.out.println("Tokio žodžio nėra, įveskite kitą žodį");
+            //Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Iveskite Key reikšmę patikrinti ar tokia egzistuoja:");
+            int key = sc.nextInt();
+            if (information.containsKey(key)) {
+                System.out.println("Toks raktas yra!");
+            } else {
+                System.out.println("Tokio rakto nėra, įveskite kitą raktą:");
+            }
+            return sc.nextInt();
+
+
         }
 
-        return sc.nextLine();
+        @Override
+        public String findStringInfo (String textInfo){
+            // Scanner scanner = new Scanner(System.in);
+            System.out.println("Kokio žodžio ieškote?");
+            String value = sc.nextLine();
+            if (information.containsValue(value)) {
+                System.out.println("Toks žodis yra");
+            } else {
+                System.out.println("Tokio žodžio nėra, įveskite kitą žodį");
+            }
+
+            return sc.nextLine();
+        }
     }
 
 }
